c84a116ee8d5a76b3a4e1a27a50e7208b7d42e04
ajf2 <ajf2@users.noreply.github.com>
2018-07-22 19:33:36 +0100

    Added a failing test.

---
+++ ApiClients/ApiClientsTests/PoliceUkApiClientTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApiClients;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApiClientsTests {
+  [TestClass]
+  public class PoliceUkApiClientTests {
+    [TestMethod]
+    public void TestAvailability() {
+      var api = new PoliceUkApiClient();
+      IEnumerable<dynamic> availability = api.GetAvailability();
+      Assert.IsTrue(availability.Count() > 0);
+    }
+  }
+}
